91a8e5b7a4e5ff00833414c972b3a8ff5b904ee5
Antikevin <kevin_3467@yahoo.com>
2025-12-06 17:14:58 -0300

    feat: Adds typeorm config module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_DATABASE || 'prodify',
+      entities: [join(__dirname, 'modules', '**', '*.entity.{ts,js}')],
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
